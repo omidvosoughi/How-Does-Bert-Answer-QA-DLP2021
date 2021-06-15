6fe0fecf5e040121ea51d4bb362e14aecc0b1786
Paul <paul_heinemeyer@outlook.de>
2021-06-16 00:17:09 +0200

    Updated visualization to work with Transformers 4.6.1 and AutoModels

--- src/visualization/pretrained_model_loader.py
+++ src/visualization/pretrained_model_loader.py
@@ -2,17 +2,16 @@ from enum import Enum
 from typing import Tuple, List
 
 import torch
-from transformers import BertForQuestionAnswering, BertTokenizer, BertConfig
-from transformers import AlbertForQuestionAnswering, AlbertTokenizer, AlbertConfig
+# from transformers import BertForQuestionAnswering, BertTokenizer, BertConfig
+# from transformers import AlbertForQuestionAnswering, AlbertTokenizer, AlbertConfig
 
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoConfig
 from .data_utils import QASample, SquadExample, QAInputFeatures, RawResult, read_squad_example, \
     convert_qa_example_to_features, parse_prediction
 
 
-class ModelFamily(Enum):
-    BERT = 1
-    ALBERT = 2
-    GPT2 = 3
+class ModelType(Enum):
+    BERT_BASE_UNCASED = "bert-base-uncased"
 
 
 """
@@ -29,44 +28,35 @@ class ModelFamily(Enum):
         cache_dir
         device
 """
+
+
 class QAModel:
 
-    def __init__(self, model_path: str, model_type: str, model_family: ModelFamily, lower_case: bool, cache_dir: str = "../cache", device: str = "cpu"):
-        self.model_path = model_path
+    def __init__(self, model_type: ModelType, lower_case: bool, custom=False, model_path=None, cache_dir: str = "../cache", device: str = "cpu"):
         self.model_type = model_type
-        self.model_family = model_family
         self.lower_case = lower_case
+        self.model_path = model_path
         self.cache_dir = cache_dir
         self.device = device
 
-        self._model = self.load_model()
+        self._model = self.load_model(custom)
         self._tokenizer = self.load_tokenizer()
 
-    def load_model(self):
-        pretrained_weights = torch.load(
-            self.model_path, map_location=torch.device(self.device))
-        if self.model_family == ModelFamily.BERT:
-            config = BertConfig.from_pretrained(
-                self.model_type, output_hidden_states=True, cache_dir=self.cache_dir)
-            model = BertForQuestionAnswering.from_pretrained(self.model_type,
-                                                             state_dict=pretrained_weights,
-                                                             config=config,
-                                                             cache_dir=self.cache_dir)
-        if self.model_family == ModelFamily.ALBERT:
-            config = AlbertConfig.from_pretrained(
-                self.model_type, output_hidden_states=True, cache_dir=self.cache_dir)
-            model = AlbertForQuestionAnswering.from_pretrained(self.model_type,
-                                                               state_dict=pretrained_weights,
-                                                               config=config,
-                                                               cache_dir=self.cache_dir)
-
+    def load_model(self, custom):
+        config = AutoConfig.from_pretrained(
+                self.model_type.value, output_hidden_states=True, cache_dir=self.cache_dir)
+        if custom:
+            pretrained_weights = torch.load(
+                self.model_path, map_location=torch.device(self.device))
+            model = AutoModelForQuestionAnswering.from_pretrained(
+                self.model_path, state_dict=pretrained_weights, config=config, cache_dir=self.cache_dir)
+        else:
+            model = AutoModelForQuestionAnswering.from_pretrained(
+                self.model_type.value, config=config, cache_dir=self.cache_dir)
         return model
 
     def load_tokenizer(self):
-        if self.model_family == ModelFamily.BERT:
-            return BertTokenizer.from_pretrained(self.model_type, cache_dir=self.cache_dir, do_lower_case=self.lower_case)
-        if self.model_family == ModelFamily.ALBERT:
-            return AlbertTokenizer.from_pretrained(self.model_type, cache_dir=self.cache_dir, do_lower_case=self.lower_case)
+        return AutoTokenizer.from_pretrained(self.model_type.value)
 
     def predict(self, sample: QASample):
         squad_formatted_sample: SquadExample = read_squad_example(sample)
@@ -80,7 +70,8 @@ class QAModel:
                       }
 
             output: Tuple = self._model(**inputs)
-            prediction, hidden_states = self.__parse_model_output(output, squad_formatted_sample, input_features)
+            prediction, hidden_states = self.__parse_model_output(
+                output, squad_formatted_sample, input_features)
 
             return prediction, hidden_states, input_features
 
@@ -92,10 +83,14 @@ class QAModel:
                                                   max_query_length=64,
                                                   is_training=False)
 
-        features.input_ids = torch.tensor([features.input_ids], dtype=torch.long)
-        features.input_mask = torch.tensor([features.input_mask], dtype=torch.long)
-        features.segment_ids = torch.tensor([features.segment_ids], dtype=torch.long)
-        features.cls_index = torch.tensor([features.cls_index], dtype=torch.long)
+        features.input_ids = torch.tensor(
+            [features.input_ids], dtype=torch.long)
+        features.input_mask = torch.tensor(
+            [features.input_mask], dtype=torch.long)
+        features.segment_ids = torch.tensor(
+            [features.segment_ids], dtype=torch.long)
+        features.cls_index = torch.tensor(
+            [features.cls_index], dtype=torch.long)
         features.p_mask = torch.tensor([features.p_mask], dtype=torch.float)
 
         return features
@@ -111,4 +106,3 @@ class QAModel:
         nbest_predictions: List = parse_prediction(sample, features, result)
 
         return nbest_predictions[0], output[2]
-
