82c7cdf7aab851f13ba5edca8ebc78340b6b71f4
SwiftPredator <paul_heinemeyer@outlook.de>
2021-06-26 15:18:53 +0000

    fine tuning T5 model + evaluation notebook

---
+++ src/utils_T5.py
@@ -0,0 +1,154 @@
+from __future__ import print_function
+from transformers import AutoTokenizer
+from dataclasses import dataclass, field
+import torch
+from typing import Dict, List, Optional
+from collections import Counter
+import string
+import re
+import argparse
+import json
+import sys
+
+
+# process the examples in input and target text format and the eos token at the end 
+def add_eos_to_examples(example):
+    example['input_text'] = 'question: %s  context: %s </s>' % (example['question'], example['context'])
+    example['target_text'] = '%s </s>' % example['answers']['text'][0]
+    return example
+
+# tokenize the examples
+def convert_to_features(example_batch, tokenizer):
+    input_encodings = tokenizer.batch_encode_plus(example_batch['input_text'], pad_to_max_length=True, max_length=512)
+    target_encodings = tokenizer.batch_encode_plus(example_batch['target_text'], pad_to_max_length=True, max_length=16)
+
+    encodings = {
+        'input_ids': input_encodings['input_ids'], 
+        'attention_mask': input_encodings['attention_mask'],
+        'target_ids': target_encodings['input_ids'],
+        'target_attention_mask': target_encodings['attention_mask']
+    }
+
+    return encodings
+
+
+def prepare_batch(batch: List) -> Dict[str, torch.Tensor]:
+    """
+    Take a list of samples from a Dataset and collate them into a batch.
+    Returns:
+        A dictionary of tensors
+    """
+    input_ids = torch.stack([example['input_ids'] for example in batch])
+    lm_labels = torch.stack([example['target_ids'] for example in batch])
+    lm_labels[lm_labels[:, :] == 0] = -100
+    attention_mask = torch.stack([example['attention_mask'] for example in batch])
+    decoder_attention_mask = torch.stack([example['target_attention_mask'] for example in batch])
+    
+
+    return {
+        'input_ids': input_ids, 
+        'attention_mask': attention_mask,
+        'labels': lm_labels, 
+        'decoder_attention_mask': decoder_attention_mask
+    }
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+    train_file_path: Optional[str] = field(
+        default='train_data.pt',
+        metadata={"help": "Path for cached train dataset"},
+    )
+    valid_file_path: Optional[str] = field(
+        default='valid_data.pt',
+        metadata={"help": "Path for cached valid dataset"},
+    )
+    max_len: Optional[int] = field(
+        default=512,
+        metadata={"help": "Max input length for the source text"},
+    )
+    target_max_len: Optional[int] = field(
+        default=32,
+        metadata={"help": "Max input length for the target text"},
+    )
+
+
+
+# Evaluation code
+# code from other source to calculate f1 score of the model; Above didnt worked even after hours of trial and error
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def f1_score(prediction, ground_truth):
+    prediction_tokens = normalize_answer(prediction).split()
+    ground_truth_tokens = normalize_answer(ground_truth).split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+
+def exact_match_score(prediction, ground_truth):
+    return (normalize_answer(prediction) == normalize_answer(ground_truth))
+
+
+def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
+    scores_for_ground_truths = []
+    for ground_truth in ground_truths:
+        score = metric_fn(prediction, ground_truth)
+        scores_for_ground_truths.append(score)
+    return max(scores_for_ground_truths)
+
+
+def evaluate(gold_answers, predictions):
+    f1 = exact_match = total = 0
+
+    for ground_truths, prediction in zip(gold_answers, predictions):
+      total += 1
+      exact_match += metric_max_over_ground_truths(
+                    exact_match_score, prediction, ground_truths)
+      f1 += metric_max_over_ground_truths(
+          f1_score, prediction, ground_truths)
+    
+    exact_match = 100.0 * exact_match / total
+    f1 = 100.0 * f1 / total
+
+    return {'exact_match': exact_match, 'f1': f1}
