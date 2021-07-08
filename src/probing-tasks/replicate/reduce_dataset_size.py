24f32b1ad297f4a969978482f3f47ac98f902a95
pschultzendorff <peter@schultzendorff.de>
2021-07-08 19:13:52 +0200

    small datasets with 500/50 samples (train/val), medium with 5000/500, big is the original size

--- src/probing-tasks/replicate/reduce_dataset_size.py
+++ src/probing-tasks/replicate/reduce_dataset_size.py
@@ -1,14 +1,34 @@
 import argparse
 import shutil
 import os
+import json
+#from tqdm import tqdm
 
-def reduce(input_dir: str) -> None:
-    for phase, limit in {"train": 7500, "val": 1500}.items():
+def reduce(input_dir: str, train_size: int=5000, val_size: int=500) -> None:
+    for phase, limit in {"train": train_size, "val": val_size}.items():
         os.makedirs(f"{input_dir}/big", exist_ok=True)
+        os.makedirs(f"{input_dir}/small", exist_ok=True)
+        os.makedirs(f"{input_dir}/medium", exist_ok=True)
         shutil.move(f"{input_dir}/{phase}.jsonl", f"{input_dir}/big/{phase}.jsonl")
         with open(f"{input_dir}/big/{phase}.jsonl", 'r') as f:
-            with open(f"{input_dir}/{phase}.jsonl", 'w') as output:
-                output.write("".join(f.readlines()[:limit]))
+            with open(f"{input_dir}/medium/{phase}.jsonl", 'w') as output:
+                counter: int = 0
+                lines = iter(f.readlines())                
+                while counter < limit:
+                    line = next(lines, None)
+                    if line is None:
+                        break
+                    counter += len(json.loads(line)["targets"])
+                    output.write(line)
+            with open(f"{input_dir}/small/{phase}.jsonl", 'w') as output:
+                counter: int = 0
+                lines = iter(f.readlines())
+                while counter < limit // 10:
+                    line = next(lines, None)
+                    if line is None:
+                        break
+                    counter += len(json.loads(line)["targets"])
+                    output.write(line)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
