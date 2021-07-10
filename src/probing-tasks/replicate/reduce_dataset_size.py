4ba535bbf7eab9d507fbbcf76d55d6f7ebb4d6f7
pschultzendorff <peter@schultzendorff.de>
2021-07-10 17:06:35 +0200

    small datasets are now correct and non-empty

--- src/probing-tasks/replicate/reduce_dataset_size.py
+++ src/probing-tasks/replicate/reduce_dataset_size.py
@@ -2,7 +2,7 @@ import argparse
 import shutil
 import os
 import json
-#from tqdm import tqdm
+from typing import List
 
 def reduce(input_dir: str, train_size: int=5000, val_size: int=500) -> None:
     for phase, limit in {"train": train_size, "val": val_size}.items():
@@ -11,20 +11,21 @@ def reduce(input_dir: str, train_size: int=5000, val_size: int=500) -> None:
         os.makedirs(f"{input_dir}/medium", exist_ok=True)
         shutil.move(f"{input_dir}/{phase}.jsonl", f"{input_dir}/big/{phase}.jsonl")
         with open(f"{input_dir}/big/{phase}.jsonl", 'r') as f:
+            lines: List[str] = f.readlines()
             with open(f"{input_dir}/medium/{phase}.jsonl", 'w') as output:
                 counter: int = 0
-                lines = iter(f.readlines())                
+                lines_iter = iter(lines)
                 while counter < limit:
-                    line = next(lines, None)
+                    line = next(lines_iter, None)
                     if line is None:
                         break
                     counter += len(json.loads(line)["targets"])
                     output.write(line)
             with open(f"{input_dir}/small/{phase}.jsonl", 'w') as output:
                 counter: int = 0
-                lines = iter(f.readlines())
-                while counter < limit // 10:
-                    line = next(lines, None)
+                lines_iter = iter(lines)
+                while counter < (limit // 10):
+                    line = next(lines_iter, None)
                     if line is None:
                         break
                     counter += len(json.loads(line)["targets"])
